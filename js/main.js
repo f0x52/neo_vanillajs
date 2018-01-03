8d13ebac99593047788083c3fb2e3038921970ab
f0x <f0x@firemail.cc>
2018-01-03 12:54:04 +0100

    bye jquery, room switching

--- js/main.js
+++ js/main.js
@@ -1,43 +1,72 @@
-$(".login").hide();
-$(".main").show();
-$("#login").submit(function() {
-    var user = $("#user").val()
-    var pass = $("#pass").val()
-    console.log("Logging in: " + user);
-    $(".login").hide();
-    $(".main").show();
-    return false;
-});
-
+hide(document.getElementsByClassName("login")[0]);
+show(document.getElementsByClassName("main")[0]);
+var prev = 0;
 var observe;
-if (window.attachEvent) {
-    observe = function (element, event, handler) {
-        element.attachEvent('on'+event, handler);
-    };
-} else {
-    observe = function (element, event, handler) {
-        element.addEventListener(event, handler, false);
-    };
+var roomid = document.querySelector('input[name="room_radio"]:checked').value
+
+function show(element) {
+    element.style.display="block"
 }
 
-var text = document.getElementById('text');
-var prev = 0;
-function resize () {
+function hide(element) {
+    element.style.display="none"
+}
+
+function login(e) {
+    e.preventDefault()
+    user = document.getElementById("user").value
+    pass = document.getElementById("pass").value
+    console.log("logging in " + user)
+
+    hide(document.getElementsByClassName("login")[0]);
+    show(document.getElementsByClassName("main")[0]);
+
+    text.focus();
+    text.select();
+    resize();
+}
+
+function resize_textarea() {
     text.style.height = 'auto';
     text.style.height = text.scrollHeight+'px';
 }
-function delayedResize () {
-    window.setTimeout(resize, 0);
+function resize_textarea_delayed () {
+    window.setTimeout(resize_textarea, 0);
 }
-observe(text, 'change',  resize);
-observe(text, 'cut',     delayedResize);
-observe(text, 'paste',   delayedResize);
-observe(text, 'drop',    delayedResize);
-observe(text, 'keydown', delayedResize);
 
-text.focus();
-text.select();
-resize();
+function start() {
+    if (window.attachEvent) {
+        observe = function (element, event, handler) {
+            element.attachEvent('on'+event, handler);
+        };
+    } else {
+        observe = function (element, event, handler) {
+            element.addEventListener(event, handler, false);
+        };
+    }
 
-console.log("Neo 0.01");
-$("#room_2").insertBefore(".room_item:first");
+    var text = document.getElementById('text');
+    observe(text, 'change',  resize_textarea);
+    observe(text, 'cut',     resize_textarea_delayed);
+    observe(text, 'paste',   resize_textarea_delayed);
+    observe(text, 'drop',    resize_textarea_delayed);
+    observe(text, 'keydown', resize_textarea_delayed);
+    observe(document.getElementById('login'), 'submit', login)
+    observe(document.getElementById('list'), 'change', roomSwitch)
+
+    roomSwitch()
+    console.log("Neo 0.01");
+}
+
+function roomSwitch() {
+    document.getElementById("messages_" + roomid).style.display="none"
+    roomid = document.querySelector('input[name="room_radio"]:checked').value
+    document.getElementById("messages_" + roomid).style.display="block"
+}
+
+function bump_room2() {
+    document.getElementById("list").insertBefore(document.getElementById("room_2"), list.childNodes[0])
+}
+
+start()
+window.setTimeout(bump_room2, 4000)
